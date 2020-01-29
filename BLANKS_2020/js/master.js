394571315671f3eb6096c017bc92ebfed5f0a84e
steve <robinson.steve@mac.com>
2020-01-29 09:53:01 -0500

    Add files via upload

---
+++ BLANKS_2020/js/master.js
@@ -0,0 +1,12 @@
+// navbar js from https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_navbar_hide_scroll
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-70px";
+  }
+  prevScrollpos = currentScrollPos;
+}
